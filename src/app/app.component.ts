fdfd06b5f577e1db71e7430f47be3a26e4e5f743
KacperPuzi0 <79982696+KacperPuzi0@users.noreply.github.com>
2022-05-02 09:45:07 +0200

    Toggle Menu Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'User-Management';
+    status: boolean = false;
+  clickEvent(){
+      this.status = !this.status;
+  }
+  dateTime = Date.now();
 }
