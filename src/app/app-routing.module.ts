a5c0364d2f1017cda1058d2711ae562fa5a7cf54
KacperPuzi0 <79982696+KacperPuzi0@users.noreply.github.com>
2022-05-02 09:57:51 +0200

    Routing Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { UsersEditComponent } from './users-edit/users-edit.component';
+import {UsersListComponent} from './users-list/users-list.component';
+import { RolesAndPermissionsComponent } from './roles-and-permissions/roles-and-permissions.component';
+import { SettingsComponent } from './settings/settings.component';
+const routes: Routes = [
+  {path:'', redirectTo:'/', pathMatch:'full'},
+  {path:'userslist', component:UsersListComponent},
+  {path:'useredit', component:UsersEditComponent},
+  {path:'rolesandpermissions', component:RolesAndPermissionsComponent},
+  {path:'settings', component:SettingsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UsersEditComponent } from './users-edit/users-edit.component';
+import { RolesAndPermissionsComponent } from './roles-and-permissions/roles-and-permissions.component';
+import { SettingsComponent } from './settings/settings.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UsersEditComponent,
+    RolesAndPermissionsComponent,
+    SettingsComponent,
+
   ],
   imports: [
     BrowserModule,
